92db50f92d7cba4d670430da0e97a0ca5703fc15
umberto <umbe@portabilix.local>
2012-08-02 09:22:52 +0200

    missing autotools

---
+++ src/include/config_F90.h
@@ -0,0 +1,74 @@
+#define BUILD_TIME "Wed Aug  1 21:51:18 CEST 2012"
+#define CC "gcc_4.4_64 -m64 -g -fbounds-check "
+#define CC_FORTRAN_INT int
+#define CFLAGS " -g -O2 -I/sw/include"
+#define F2003_C_PTR 1
+#define F90_ACCEPTS_LINE_NUMBERS 1
+#define FC "gfortran_4.4_64 -m64 -g -fbounds-check "
+#define FCFLAGS "-O3 -I/Users/umbe/include -L/Users/umbe/lib "
+#define FC_COMMAND_LINE_ARGUMENTS 2003
+#define FC_FUNC(name,NAME) name ## _
+#define FC_FUNC_(name,NAME) name ## _
+#define FC_INTEGER_SIZE 4
+#define HAVE_ALLOCA 1
+#define HAVE_ALLOCA_H 1
+#define HAVE_ALPHASORT 1
+#define HAVE_ARPACK 1
+#define HAVE_BLAS 1
+#define HAVE_BUILTIN_PREFETCH 1
+#define HAVE_CLOSEDIR 1
+#define HAVE_DIRENT_H 1
+#define HAVE_ERRNO_H 1
+#define HAVE_FC_SIZEOF 1
+#define HAVE_FLUSH 1
+#define HAVE_FORTRAN_LOC 1
+#define HAVE_GETOPT_LONG 1
+#define HAVE_GETTIMEOFDAY 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_IOCTL 1
+#define HAVE_LAPACK 1
+#define HAVE_LIBXC 1
+#define HAVE_M128D 1
+#define HAVE_MEMORY_H 1
+#define HAVE_NETCDF 1
+#define HAVE_PERROR 1
+#define HAVE_READDIR 1
+#define HAVE_SBRK 1
+#define HAVE_SCANDIR 1
+#define HAVE_SIGACTION 1
+#define HAVE_SIGNAL_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRCASESTR 1
+#define HAVE_STRCHR 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_STRTOD 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_TCGETPGRP 1
+#define HAVE_UINT32_T 1
+#define HAVE_UINT64_T 1
+#define HAVE_UNAME 1
+#define HAVE_UNISTD_H 1
+#define HAVE_VEC 1
+#define LATEST_SVN "9223"
+#define MAX_DIM 3
+#define OCT_ARCH x86_64
+#define OCT_ARCH_X86_64 1
+#define PACKAGE "octopus"
+#define PACKAGE_BUGREPORT "octopus-devel@tddft.org"
+#define PACKAGE_NAME "Octopus"
+#define PACKAGE_STRING "Octopus nocturnus"
+#define PACKAGE_TARNAME "octopus"
+#define PACKAGE_URL ""
+#define PACKAGE_VERSION "nocturnus"
+#define SIZEOF_SIZE_T 8
+#define SIZEOF_UNSIGNED_INT 4
+#define SIZEOF_UNSIGNED_LONG 8
+#define SIZEOF_UNSIGNED_LONG_LONG 8
+#define SIZEOF_VOIDP 8
+#define STDC_HEADERS 1
+#define TIME_WITH_SYS_TIME 1
+#define VERSION "nocturnus"
+#define restrict __restrict
